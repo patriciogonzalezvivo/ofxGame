788e54c94b5ef6a01d0b9640fd7bf6d56c6f1a24
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2011-07-14 09:20:44 -0300

    ofxMask now support matrix calibration in order to make ofxOpenNI user masking work well under linux && adding imageSequence in order to have alpha videos working properly

--- src/ofxMask.h
+++ src/ofxMask.h
@@ -1,8 +1,15 @@
 /*
- *  ofxUser.h
+ *  ofxMask.h
  *
  *  Created by Patricio González Vivo on 30/06/11.
  *  Copyright 2011 PatricioGonzalezVivo.com. All rights reserved.
+ *
+ *  It´s posible to use a transformation matrix in order to calibrate the mask width the image. 
+ *  For example the getUserPixels function of ofxUserGenerator of ofxOpenNI doesn´t work well
+ *	on linux. In that case you have to make a transformation matrix using this app:
+ *  
+ *  https://github.com/patriciogonzalezvivo/homography4ofxOpenNI
+ *
  *
  *  This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 
  *	Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
@@ -24,32 +31,73 @@ public:
 			width = 640;
 		if ( !(height > 0) )
 			height = 480;
-			usersMasked.allocate(width, height, OF_IMAGE_COLOR_ALPHA);
-		pAverage = 0;
+		
+		if (file != "none")
+			loadMatrix(file);
+		
+		usersMasked.allocate(width, height, OF_IMAGE_COLOR_ALPHA);
 	};
+	
+	ofxMask& loadMatrix(string _file){
+		ofxXmlSettings xml;
 		
+		if (xml.loadFile(_file)){
+			xml.pushTag("matrix");
+			for(int i=0;i<16;i++){
+				tMat.getPtr()[i] = xml.getValue("m_"+ofToString(i),0.);
+			}
+			xml.popTag();
+		}
+		return * this;
+	}
+	
 	void update(unsigned char * mskPixels, unsigned char * imagePixels, int blurAmount = 0){
-		pAverage = 0;
-		
 		if (blurAmount > 0)
 			superFastBlur(mskPixels, blurAmount);
 		
 		unsigned char * fPixels = usersMasked.getPixels();
 		
-		for( int i=0; i < width; i++ ){
-			for( int j=0; j < height; j++ ){
-				int p = j * width + i;	
-				fPixels[p * 4 + 0] = imagePixels[p * 3 + 0];
-				fPixels[p * 4 + 1] = imagePixels[p * 3 + 1];
-				fPixels[p * 4 + 2] = imagePixels[p * 3 + 2];
-				fPixels[p * 4 + 3] = mskPixels[p];	
-				
-				if (mskPixels[p] > 100)
-					pAverage++;				//This is handy for using users masks like triggers
+		if (file != none){
+			// MASKING WIDTH MATRIX TRANSFORMATION MANUAL CALIBRATION
+			ofVec3f texCoord3D;
+			for( int x = 0; x < width; x++ ){
+				for( int y = 0; y < height; y++ ){
+					int p = y * width + x;
+					
+					if (mskPixels[p] > 10){		// Just the need pixels... this is faster??
+						texCoord3D.set(x,y,0);
+						texCoord3D = tMat * texCoord3D;
+						texCoord3D.x = ofClamp(texCoord3D.x,0,width);
+						texCoord3D.y = ofClamp(texCoord3D.y,0,height);
+						
+						int hP = (int)(texCoord3D.y) * width + (int)(texCoord3D.x);
+						
+						fPixels[p * 4 + 0] = imagePixels[hP * 3 + 0];
+						fPixels[p * 4 + 1] = imagePixels[hP * 3 + 1];
+						fPixels[p * 4 + 2] = imagePixels[hP * 3 + 2];
+						fPixels[p * 4 + 3] = mskPixels[p];	
+						
+					} else {
+						fPixels[p * 4 + 0] = mskPixels[p];
+						fPixels[p * 4 + 1] = mskPixels[p];
+						fPixels[p * 4 + 2] = mskPixels[p];
+						fPixels[p * 4 + 3] = mskPixels[p];
+					}
+				}
+			}
+		} else {
+			// DIRECT MASKING
+			for( int i=0; i < width; i++ ){
+				for( int j=0; j < height; j++ ){
+					int p = j * width + i;	
+					fPixels[p * 4 + 0] = imagePixels[p * 3 + 0];
+					fPixels[p * 4 + 1] = imagePixels[p * 3 + 1];
+					fPixels[p * 4 + 2] = imagePixels[p * 3 + 2];
+					fPixels[p * 4 + 3] = mskPixels[p];				
+				}
 			}
 		}
 		
-		pAverage /= width*height;
 		usersMasked.setFromPixels( fPixels , width, height, OF_IMAGE_COLOR_ALPHA);
 	};
 	
@@ -68,14 +116,7 @@ public:
 		ofPopMatrix();
 	};
 	
-	float	getAverage(){return pAverage;};
-	int		getMaskWidth(){return usersMasked.getWidth();};
-	int		getMaskHeight(){return usersMasked.getHeight();};
-	unsigned char * getPixels(){return usersMasked.getPixels();};
-	
 private:
-	float pAverage;
-	
 	void superFastBlur(unsigned char *pix, int radius){  
 		int w = width;
 		int h = height;
@@ -146,7 +187,8 @@ private:
 		delete vMAX;  
 		delete dv;  
 	};
-
-	ofImage				usersMasked;
+	
+	ofImage		usersMasked;
+	ofMatrix4x4 tMat;
 };
 #endif
